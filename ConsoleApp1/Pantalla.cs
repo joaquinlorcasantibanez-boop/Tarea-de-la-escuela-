f7cbe8f8c0cff93406835929d385ad571419d9bc
joaquinlorcasantibanez-boop <joaquin.lorca.santibanez@gmail.com>
2026-09-21 03:12:00 -0300

    Add files via upload

---
+++ ConsoleApp1/Pantalla.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace TuProyecto
+{
+    public class Pantalla
+    {
+        public void BuscarTrabajador()
+        {
+            Console.Write("Ingrese el ID del trabajador: ");
+
+            int idTrabajador = int.Parse(Console.ReadLine());
+
+            Conexion conexion = new Conexion();
+
+            using (SqlConnection cn = conexion.Conectar())
+            {
+                SqlCommand cmd =
+                    new SqlCommand("SP_BuscarTrabajadorPorId", cn);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue(
+                    "@IdTrabajador",
+                    idTrabajador);
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    Console.WriteLine("\n=== DATOS DEL TRABAJADOR ===");
+
+                    Console.WriteLine(
+                        $"Nombre: {dr["Nombre"]}");
+
+                    Console.WriteLine(
+                        $"Correo: {dr["Correo"]}");
+
+                    Console.WriteLine(
+                        $"Celular: {dr["Celular"]}");
+
+                    Console.WriteLine(
+                        $"Cargo: {dr["Cargo"]}");
+                }
+                else
+                {
+                    Console.WriteLine(
+                        "No se encontró ningún trabajador con ese ID.");
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Reflection.Metadata;
+using TuProyecto;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sistema para modificar estado.");
+            Console.WriteLine("¿Qué quiere hacer?");
+            Console.WriteLine("1. Modificar estado del caso.");
+            Console.WriteLine("2. Buscar trabajadores.");
+            Console.WriteLine("3. Añadir clientes");
+            Console.WriteLine("4. Salir");
+
+            Contador_de_Intentos contador = new Contador_de_Intentos(4);
+            int opcion = contador.LeerOpcion();
+
+            Pantalla pantalla = new Pantalla();
+
+            switch (opcion)
+            {
+                case 1:
+                    Casos.ActualizarEstadoCaso();
+                    break;
+
+                case 2:
+                    pantalla.BuscarTrabajador();
+                    break;
+
+                case 3:
+                    Clientes.InsertarClienteYCaso();
+                    break;
+
+                case 4:
+                    Console.WriteLine("Saliendo...");
+                    break;
+
+                default:
+                    Console.WriteLine("Opción no válida.");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+   
